38bc5e673674a1c44504f3796cb335a5a6f83285
joey.cheng <jemicheng@gmail.com>
2023-07-27 07:48:23 +0000

    refactor: refactor RTSP player implementation
    - The `rtspplayer` file was deleted.
    - Changes were made to the `rtspplayer.cpp` file.
    - Added code related to the `FFMPEG_HELPER` flag.
    - Added a function `isIFrame` to check if a frame is an I-frame.
    - Modified the `onFrameArrival` function to handle different types of frames.
    - Added code related to the `onConnectionSetup` function.
    - Removed the `main` function.
    - Added code to free the `pCodecCtx` context.
    
    Signed-off-by: joey.cheng <jemicheng@gmail.com>

--- app/rtspplayer.cpp
+++ app/rtspplayer.cpp
@@ -1,15 +1,105 @@
 #include "rtspClientHelper.hh"
 
-void onFrameArrival(unsigned char *clientData, const char *codecName, unsigned frameSize, unsigned numTruncatedBytes, struct timeval presentationTime)
+#define FFMPEG_HELPER 1
+
+#ifdef FFMPEG_HELPER
+extern "C"
 {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+}
+AVCodecContext *pCodecCtx = NULL;
+bool isIFrame(AVPacket *packet)
+{
+    int got_frame;
+    if (!pCodecCtx)
+        return false;
+    AVFrame *frame = av_frame_alloc();
+
+    avcodec_send_packet(pCodecCtx, packet);
+    got_frame = avcodec_receive_frame(pCodecCtx, frame);
+
+    bool isIframe = false;
+    if (got_frame != AVERROR(EAGAIN) && got_frame != AVERROR_EOF)
+    {
+        isIframe = frame->pict_type == AV_PICTURE_TYPE_I;
+    }
+    av_frame_free(&frame);
+    return isIframe;
+}
+#endif
+
+void onFrameArrival(unsigned char *videoData, const char *codecName, unsigned frameSize, unsigned numTruncatedBytes, struct timeval presentationTime)
+{
+#ifdef FFMPEG_HELPER
+    char uSecsStr[7];
+    u_int8_t start_code[4] = {0x00, 0x00, 0x00, 0x01};
+    u_int8_t *frameData = (u_int8_t *)malloc(frameSize + 4);
+    AVPacket packet;
+    memcpy(frameData, start_code, 4);
+    memcpy(frameData + 4, videoData, frameSize);
+    av_new_packet(&packet, frameSize + 4);
+    packet.data = frameData; //(uint8_t*)fReceiveBuffer;
+    packet.size = frameSize + 4;
+    bool isIframe = isIFrame(&packet);
+    snprintf(uSecsStr, 7, "%06u", (unsigned)presentationTime.tv_usec);
+    if (isIframe)
+    {
+        std::cout << " codec:" << codecName << " I-Frame "
+                << " size:" << frameSize << " bytes "
+                << "presentation time:" << (int)presentationTime.tv_sec << "." << uSecsStr << "\n";
+    }
+    else
+    {
+        std::cout << " codec:" << codecName << " P-frame "
+                << " size:" << frameSize << " bytes "
+                << " presentation time:" << (int)presentationTime.tv_sec << "." << uSecsStr << "\n";
+    }
+    av_packet_unref(&packet);
+#else
     char uSecsStr[7];
     snprintf(uSecsStr, 7, "%06u", (unsigned)presentationTime.tv_usec);
     std::cout << "codec name:" << codecName << " frame size:" << frameSize << " presentationTime:" << presentationTime.tv_sec << "." << uSecsStr << std::endl;
+#endif
 }
 
 void onConnectionSetup(char *codecName)
 {
     std::cout << "codec:" << codecName << std::endl;
+#ifdef FFMPEG_HELPER
+    AVCodec *codec;
+    if (strcmp(codecName, "H264") == 0)
+        codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+    else if (strcmp(codecName, "H265") == 0)
+        codec = avcodec_find_decoder(AV_CODEC_ID_H265);
+    else
+    {
+        std::cout << "Failed to find the suitable decoder"
+            << "\n";
+        return;
+    }
+    if (!codec)
+    {
+        std::cout << "Failed to find the suitable decoder"
+            << "\n";
+        return;
+    }
+    pCodecCtx = avcodec_alloc_context3(codec);
+    if (!pCodecCtx)
+    {
+        std::cout << "Failed to find the suitable context"
+            << "\n";
+        return;
+    }
+    // Open the codec
+    if (avcodec_open2(pCodecCtx, codec, NULL) < 0)
+    {
+        std::cout << "Failed to open the codec"
+            << "\n";
+        avcodec_free_context(&pCodecCtx);
+        return;
+    }
+#endif
 }
 
 int main(int argc, char *argv[])
@@ -23,4 +113,7 @@ int main(int argc, char *argv[])
         player->stopRTSP();
     }
     delete (player);
+#ifdef FFMPEG_HELPER
+    avcodec_free_context(&pCodecCtx);
+#endif
 }
